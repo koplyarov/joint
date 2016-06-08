a7a45066c49c844499968bba1bffc478d536b4a0
Dmitry Koplyarov <dmitry.koplyarov@iksatprof.ru>
2016-06-08 19:23:59 +0300

    IdlParser

--- bin/joint/IdlParser.py
+++ bin/joint/IdlParser.py
@@ -1,3 +1,24 @@
+from pyparsing import *
+
 class IdlParser:
     def __init__(self):
-        print "hello"
+        type = Word(alphas, alphanums + '_')
+        identifier = Word(alphas, alphanums + '_')
+
+        generic = Suppress('<') + (type + ZeroOrMore(Suppress(',') + type))('params') + Suppress('>')
+
+        param = type('type') + identifier('name')
+        paramsList = Optional(Group(param) + ZeroOrMore(Suppress(',') + Group(param)))
+        method = type('retType') + identifier('name') + Optional(generic)('generic') + Suppress('(') + Group(paramsList)('params') + Suppress(');')
+
+        methodList = ZeroOrMore(Group(method))
+        interface = Suppress('interface') + identifier('name') + Suppress('{') + Group(methodList)('methods') + Suppress('}')
+
+        packageName = Group(identifier + ZeroOrMore(Suppress('.') + identifier))
+        package = Suppress('package') + packageName('packageName') + Suppress('{') + Group(ZeroOrMore(Group(interface)))('interfaces') + Suppress('}')
+
+        self.grammar = package
+        self.grammar.ignore(cppStyleComment)
+
+    def parseFile(self, file):
+        return self.grammar.parseFile(file).asDict()
--- bin/test.py
+++ bin/test.py
@@ -3,3 +3,4 @@
 import joint
 
 idl_parser = joint.IdlParser()
+print idl_parser.parseFile("core/idl/IObject.idl")
