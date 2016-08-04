234c717f6de96fce55b59f82bbf1d747955cd620
Dmitry Koplyarov <koplyarov.da@gmail.com>
2016-08-04 20:07:14 +0300

    Integrating CPack

---
+++ bin/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='joint-gen',
+        version='0.0',
+        description='Joint adapters generator',
+        author='Dmitry Koplyarov',
+        author_email='koplyarov.da@gmail.com',
+        url='https://github.com/koplyarov/joint',
+        packages=['joint', 'joint.generators'],
+        scripts=['joint-gen.py']
+     )
