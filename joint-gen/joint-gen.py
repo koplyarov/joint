8c07c973ad67ffd1e11817c4319e2e4565f39858
Dmitry Koplyarov <koplyarov.da@gmail.com>
2016-08-05 18:35:09 +0300

    Added idl directory to joint-gen package

--- joint-gen/joint-gen.py
+++ joint-gen/joint-gen.py
@@ -18,17 +18,23 @@ generators = {
 
 parser = argparse.ArgumentParser(description='Joint adapters generator')
 parser.add_argument('--language', '-l', dest='languageId', help='Output language', required=True)
-parser.add_argument('--import-dir', '-I', action='append', dest='importDir', help='A directory for imported IDLs lookup', required=True)
+parser.add_argument('--import-dir', '-I', action='append', dest='importDir', help='A directory for imported IDLs lookup')
 parser.add_argument('--output', '-o', dest='output', help='Output file', required=True)
 parser.add_argument('input', nargs='+', help='IDL files to process')
 
 args = parser.parse_args()
 
+importDirs = args.importDir
+if not importDirs:
+    importDirs = []
+importDirs.append('{}/idl'.format(os.path.dirname(os.path.realpath(__file__))))
+importDirs.append('/usr/share/joint/idl')
+
 try:
     if not args.languageId in generators:
         raise CmdLineException('Unknown language id: {}'.format(args.languageId))
 
-    semantic_graph_builder = joint.SemanticGraphBuilder(args.importDir)
+    semantic_graph_builder = joint.SemanticGraphBuilder(importDirs)
 
     gen = generators[args.languageId](semantic_graph_builder.build(args.input))
     out_file = open(args.output, 'w')
