e7a2abc9cccd6795a745809a2e9cb2a148d221f4
Dmitry Koplyarov <koplyarov.da@gmail.com>
2016-08-24 23:02:34 +0300

    Some modifications in IdlParser

--- joint-gen/joint/IdlParser.py
+++ joint-gen/joint/IdlParser.py
@@ -13,27 +13,31 @@ class IdlParser:
         import_entry = Group(Suppress(Keyword('import')) - self.locator + SkipTo(LineEnd())('path'))
         imports = ZeroOrMore(import_entry)('imports')
 
-        type = self.locator + (Word(alphas, alphanums) + ZeroOrMore(Suppress('.') - Word(alphas, alphanums)))('name')
+        type = Group(self.locator + (Word(alphas, alphanums) + ZeroOrMore(Suppress('.') - Word(alphas, alphanums)))('name'))
         identifier = Word(alphas, alphanums)
 
         generic = Suppress('<') + (type + ZeroOrMore(Suppress(',') + type))('params') + Suppress('>')
 
-        param = type('type') + identifier('name')
-        paramsList = Optional(Group(param) + ZeroOrMore(Suppress(',') + Group(param)))
-        method = type('retType') + identifier('name') + Optional(generic)('generic') + Suppress('(') + Group(paramsList)('params') + Suppress(');')
-        methodList = ZeroOrMore(Group(method))
+        param = Group(self.locator + type('type') + identifier('name'))
+        paramsList = Group(Optional(param + ZeroOrMore(Suppress(',') + param)))
+        method = Group(self.locator + type('retType') + identifier('name') + Optional(generic)('generic') + Suppress('(') + paramsList('params') + Suppress(');'))
+        methodList = Group(ZeroOrMore(method))
 
         package = Group(identifier + ZeroOrMore(Suppress('.') + identifier))
 
-        basesList = Group(type) + ZeroOrMore(Suppress(',') + Group(type))
-        interface = self.locator + Keyword('interface')('kind') - identifier('name') + Optional(Suppress(':') - basesList)('bases') + Suppress('{') + Group(methodList)('methods') + Suppress('}')
+        basesList = type + ZeroOrMore(Suppress(',') + type)
+        interface = Group(self.locator + Keyword('interface')('kind') - identifier('name') + Optional(Suppress(':') - basesList)('bases') + Suppress('{') + methodList('methods') + Suppress('}'))
 
         integerConstant = Word(nums).setParseAction(lambda s, l, t: int(t[0]))
-        enumValue = self.locator + identifier('name') - Optional(Suppress('=') + integerConstant('value'))
-        enumValuesList = Optional(Group(enumValue) + ZeroOrMore(Suppress(',') + Group(enumValue)))
-        enum = self.locator + Keyword('enum')('kind') - identifier('name') + Suppress('{') + Group(enumValuesList)('values') + Suppress('}')
+        enumValue = Group(self.locator + identifier('name') - Optional(Suppress('=') + integerConstant('value')))
+        enumValuesList = Group(Optional(enumValue + ZeroOrMore(Suppress(',') + enumValue)))
+        enum = Group(self.locator + Keyword('enum')('kind') - identifier('name') + Suppress('{') + enumValuesList('values') + Suppress('}'))
 
-        package = Suppress(Keyword('package')) + package('package') + Suppress('{') + Group(ZeroOrMore(Group(interface) | Group(enum)))('types') + Suppress('}')
+        structMember = Group(self.locator + type('type') + identifier('name') + Suppress(';'))
+        structMembersList = Group(ZeroOrMore(structMember))
+        struct = Group(self.locator + Keyword('struct')('kind') - identifier('name') + Suppress('{') + structMembersList('members') + Suppress('}'))
+
+        package = Suppress(Keyword('package')) + package('package') + Suppress('{') + Group(ZeroOrMore(interface | enum | struct))('types') + Suppress('}')
 
         self.grammar = imports + package
         self.grammar.ignore(cppStyleComment)
