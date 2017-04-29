f1d3bc0c353ca60afd45edd8c1a6a55ac0a2aa10
Dmitry Koplyarov <koplyarov.da@gmail.com>
2017-04-29 21:19:17 +0300

    Fixing benchmarks

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PYTHONPATH="`pwd`/build/bin:`pwd`/benchmarks/python:`pwd`/benchmarks/swig"
+export PYTHONPATH="`pwd`/build/bin:`pwd`/benchmarks/swig"
 clear
 ./build/bin/joint-benchmarks -c3 -v2 -t `pwd`/benchmarks/template.md -o-
