696fbad068955c23a8ab3c682840d9f89355de92
koplyarov <koplyarov.da@gmail.com>
2017-09-21 02:59:46 +0300

    Exclude some more paths from coveralls

--- ci/send_coverage.sh
+++ ci/send_coverage.sh
@@ -3,6 +3,6 @@
 echo "Sending coverage info"
 
 lcov --directory . --capture --output-file coverage.info &&
-lcov --remove coverage.info '/usr/*' 'test/*' 'benchmarks/*' 'examples/*' --output-file coverage.info &&
+lcov --remove coverage.info '/usr/*' 'test/*' 'benchmarks/*' 'examples/*' 'build/*' 'bindings/java/*' --output-file coverage.info &&
 lcov --list coverage.info && coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info ||
 exit 1
