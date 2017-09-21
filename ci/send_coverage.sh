de063477f8d5bb0b492f5e130fcb800cd09d292c
koplyarov <koplyarov.da@gmail.com>
2017-09-21 23:11:57 +0300

    Codecov integration

--- ci/send_coverage.sh
+++ ci/send_coverage.sh
@@ -2,7 +2,23 @@
 
 echo "Sending coverage info"
 
+ToCoveralls() {
+	coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info || ( echo "Coveralls did not collect coverage reports"; return 1 )
+}
+
+ToCodecov() {
+	bash <(curl -s https://codecov.io/bash) || ( echo "Codecov did not collect coverage reports"; return 1 )
+}
+
+Upload() {
+	local RET=0
+	ToCodecov || RET=1
+	ToCoveralls || RET=1
+	return $RET
+}
+
 lcov --directory . --capture --output-file coverage.info &&
 lcov --remove coverage.info '/usr/*' 'test/*' 'benchmarks/*' 'examples/*' 'build/core/joint/JsonParser.*.cpp' 'bindings/java/*' --output-file coverage.info &&
-lcov --list coverage.info && coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info ||
+lcov --list coverage.info && 
+Upload ||
 exit 1
