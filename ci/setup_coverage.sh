7b80e2c8271148abfb44a38e03abd65a0d913091
koplyarov <koplyarov.da@gmail.com>
2017-09-22 22:04:07 +0300

    Fixing Travis CI

--- ci/setup_coverage.sh
+++ ci/setup_coverage.sh
@@ -12,9 +12,11 @@ InstallJinja2Coverage() {
 	Verbose mkdir thirdparty &&
 	Verbose cd thirdparty &&
 	Verbose git clone https://github.com/MrSenko/coverage-jinja-plugin.git &&
-	cd coverage-jinja-plugin &&
-	Verbose git checkout e074018d3e3854bf39a588ef62abc4936c43c2a4 &&
-	Verbose sudo python setup.py install
+	(
+		cd coverage-jinja-plugin &&
+		Verbose git checkout e074018d3e3854bf39a588ef62abc4936c43c2a4 &&
+		Verbose sudo python setup.py install
+	)
 }
 
 Verbose InstallJinja2Coverage &&
