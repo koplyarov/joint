016e558c54b0acc39b040190277b002a4f0ff1d4
koplyarov <koplyarov.da@gmail.com>
2017-09-22 21:45:50 +0300

    Trying to add jinja2 templates to test coverage analysis

---
+++ ci/setup_coverage.sh
@@ -0,0 +1,26 @@
+[ "$TEST_COVERAGE" -eq 0 ] && exit 0
+
+Verbose() {
+	$@
+	if [ $? -ne 0 ]; then
+		echo "$* failed"
+		return 1
+	fi
+}
+
+InstallJinja2Coverage() {
+	Verbose mkdir thirdparty &&
+	Verbose cd thirdparty &&
+	Verbose git clone https://github.com/MrSenko/coverage-jinja-plugin.git &&
+	Verbose git checkout e074018d3e3854bf39a588ef62abc4936c43c2a4 &&
+	Verbose sudo python setup.py install
+}
+
+Verbose InstallJinja2Coverage &&
+Verbose echo "[run]
+plugins =
+    jinja_coverage
+
+[jinja_coverage]
+template_directory = $(pwd)/joint-gen/joint/templates" > .coveragerc ||
+exit 1
