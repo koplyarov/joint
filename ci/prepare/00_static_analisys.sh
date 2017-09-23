1ad02b6afee6f3d2096b1e5c4c91ea1d8a1cc3e0
koplyarov <koplyarov.da@gmail.com>
2017-09-23 23:45:53 +0300

    Reduce verbosity of wget in CI scripts

--- ci/prepare/00_static_analisys.sh
+++ ci/prepare/00_static_analisys.sh
@@ -3,6 +3,6 @@
 ARCHIVE="coverity_tool.tgz"
 
 WithMsg "Testing token" test "${COVERITY_SCAN_TOKEN}" &&
-WithMsg "Downloading coverity scan tool" wget https://scan.coverity.com/download/linux64 --post-data "token=${COVERITY_SCAN_TOKEN}&project=koplyarov%2Fjoint" -O "$ARCHIVE" &&
+WithMsg "Downloading coverity scan tool" wget --no-verbose https://scan.coverity.com/download/linux64 --post-data "token=${COVERITY_SCAN_TOKEN}&project=koplyarov%2Fjoint" -O "$ARCHIVE" &&
 Verbose tar -xvzf "$ARCHIVE" ||
 exit 1
