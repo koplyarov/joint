8eff2efcf052a6b6318ee4d99b9422f169b8e80e
koplyarov <koplyarov.da@gmail.com>
2017-09-23 23:48:19 +0300

    Fix ci/finalize/00_static_analysis.sh issues

--- ci/finalize/00_static_analysis.sh
+++ ci/finalize/00_static_analysis.sh
@@ -3,10 +3,12 @@
 ARCHIVE="joint.tgz"
 
 WithMsg "Testing token" test "${COVERITY_SCAN_TOKEN}" &&
+Verbose cd build &&
 Verbose tar czvf "$ARCHIVE" cov-int &&
-WithMsg "Uploading Coverirty result" curl \
+WithMsg "Uploading Coverity result" curl \
 	--form token="${COVERITY_SCAN_TOKEN}" \
 	--form email=koplyarov.da@gmail.com \
 	--form file="@$ARCHIVE" \
 	--form version="$(date --iso-8601)" \
-	"https://scan.coverity.com/builds?project=koplyarov%2Fjoint"
+	"https://scan.coverity.com/builds?project=koplyarov%2Fjoint" ||
+exit 1
