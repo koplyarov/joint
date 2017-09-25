a2a61028407ba58d110856349c19861f76a95198
koplyarov <koplyarov.da@gmail.com>
2017-09-25 17:06:03 +0300

    Increase CI benchmarks verbosity

--- ci/run/01_benchmarks.sh
+++ ci/run/01_benchmarks.sh
@@ -1,5 +1,5 @@
 [ "$RUN_BENCHMARKS" -ne 0 ] || exit 0
 
-Verbose sudo PYTHONPATH="`pwd`/build/bin:`pwd`/benchmarks/implementations/swig" ./build/bin/joint-benchmarks -c10 -v2 -t benchmarks/ci/template.py -o- > build/benchmarks/ci.py && 
+Verbose sudo PYTHONPATH="`pwd`/build/bin:`pwd`/benchmarks/implementations/swig" ./build/bin/joint-benchmarks -c10 -v3 -t benchmarks/ci/template.py -o- > build/benchmarks/ci.py && 
 Verbose python ./build/benchmarks/ci.py --data benchmarks/ci/canon_data.json ||
 exit 1
