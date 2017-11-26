9193934830c77ca4ef2426ebf8d90f6b839ad0bd
Dmitry Koplyarov <koplyarov.da@gmail.com>
2017-11-26 18:46:42 +0300

    Fix CI benchmarks

--- ci/run/01_benchmarks.sh
+++ ci/run/01_benchmarks.sh
@@ -2,10 +2,11 @@
 
 [ "$BENCHMARKS_NUM_PASSES" ] || BENCHMARKS_NUM_PASSES=2
 
-export PYTHONPATH="$BUILD_DIR/bin"
 Verbose "$SOURCE_DIR/benchmarks/core/bin/benchmarks-ci.py" \
     --executable "$MAIN_DIR/build/bin/joint-benchmarks" \
     --reference-executable "$REFERENCE_JOINT_DIR/build/bin/joint-benchmarks" \
+    --env-update "{\"PYTHONPATH\": \"$MAIN_DIR/build/bin/bin\"}"
+    --reference-env-update "{\"PYTHONPATH\": \"$REFERENCE_JOINT_DIR/build/bin/bin\"}"
     --benchmarks "$SOURCE_DIR/benchmarks/ci/benchmarks.json" \
     --num-passes "$BENCHMARKS_NUM_PASSES" ||
 exit 1
