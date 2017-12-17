f4def4f84a4aefae5611ab7c7fc1c7a767f15569
Dmitry Koplyarov <koplyarov.da@gmail.com>
2017-12-17 21:02:41 +0300

    Add PrintDiskStats to ci/prepare/00_static_analysis.sh

--- ci/prepare/00_static_analysis.sh
+++ ci/prepare/00_static_analysis.sh
@@ -2,7 +2,14 @@
 
 ARCHIVE="coverity_tool.tgz"
 
+PrintDiskStats() {
+    df -h .
+}
+
 WithMsg "Testing token" test "${COVERITY_SCAN_TOKEN}" &&
+Verbose PrintDiskStats &&
 WithMsg "Downloading coverity scan tool" wget --no-verbose https://scan.coverity.com/download/linux64 --post-data "token=${COVERITY_SCAN_TOKEN}&project=koplyarov%2Fjoint" -O "$ARCHIVE" &&
-Verbose tar -xvzf "$ARCHIVE" ||
+Verbose PrintDiskStats &&
+Verbose tar -xvzf "$ARCHIVE" &&
+Verbose PrintDiskStats ||
 exit 1
