cc6dbe3420e01308ce898ab6cc298efeb861823a
koplyarov <koplyarov.da@gmail.com>
2017-12-18 13:37:59 +0300

    Remove PrintDiskStats from ci/prepare/00_static_analysis.sh

--- ci/prepare/00_static_analysis.sh
+++ ci/prepare/00_static_analysis.sh
@@ -2,14 +2,7 @@
 
 ARCHIVE="coverity_tool.tgz"
 
-PrintDiskStats() {
-    df -h .
-}
-
 WithMsg "Testing token" test "${COVERITY_SCAN_TOKEN}" &&
-Verbose PrintDiskStats &&
 WithMsg "Downloading coverity scan tool" wget --no-verbose https://scan.coverity.com/download/linux64 --post-data "token=${COVERITY_SCAN_TOKEN}&project=koplyarov%2Fjoint" -O "$ARCHIVE" &&
-Verbose PrintDiskStats &&
-Verbose tar -xzf "$ARCHIVE" &&
-Verbose PrintDiskStats ||
+Verbose tar -xzf "$ARCHIVE" ||
 exit 1
