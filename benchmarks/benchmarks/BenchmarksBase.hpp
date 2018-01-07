e7c5e30878b7cb7e25209845f7ca288806ac39ec
koplyarov <koplyarov.da@gmail.com>
2018-01-07 20:43:32 +0300

    numWarmUpPassses benchmark parameter

--- benchmarks/benchmarks/BenchmarksBase.hpp
+++ benchmarks/benchmarks/BenchmarksBase.hpp
@@ -14,6 +14,7 @@ namespace benchmarks
     public:
         using BenchmarkCtx = typename Desc_::BenchmarkCtx;
         using CreateBenchmarksFunc = std::function<BenchmarkPtr_(const BenchmarkCtx&)>;
+        using SimpleBenchmarkFunc = std::function<void(BenchmarkContext& context, const std::string& language, size_t numWarmUpPasses)>;
 
     private:
         CreateBenchmarksFunc     _createBenchmarks;
@@ -26,20 +27,20 @@ namespace benchmarks
     protected:
         template < typename Func_ >
         void AddSimpleBenchmark(const std::string& name, const Func_& func)
-        { AddBenchmark<std::string>(name, MakeSimple(func), {"lang"}); }
+        { AddBenchmark<std::string, size_t>(name, MakeSimple(func), {"lang", "numWarmUpPasses"}, {{"numWarmUpPasses", "1"}}); }
 
         template < typename Func_ >
-        std::function<void(BenchmarkContext& context, const std::string& language)> MakeSimple(const Func_& func)
+        SimpleBenchmarkFunc MakeSimple(const Func_& func)
         {
             auto cb = _createBenchmarks;
-            return [func, cb](BenchmarkContext& context, const std::string& language) {
+            return [func, cb](BenchmarkContext& context, const std::string& language, size_t numWarmUpPasses) {
                 const auto n = context.GetIterationsCount();
                 BenchmarkCtx ctx(language);
                 auto b = cb(ctx);
                 if (!b)
                     throw std::runtime_error("Could not create Benchmarks object!");
 
-                context.WarmUpAndProfile("main", n, [&]{ func(ctx, n, b); });
+                context.WarmUpAndProfile("main", n, [&]{ func(ctx, n, b); }, numWarmUpPasses);
             };
         }
     };
